d48e02091f67dc7e01b64d2d842a144fbb660a08
Mujaahid <hackm680@gmail.com>
2026-04-07 15:17:42 +0300

    You Can't Code Under Pressure #1.

---
+++ 28. Challenge-28.js
@@ -0,0 +1,12 @@
+// You Can't Code Under Pressure #1
+
+/*
+Code as fast as you can! You need to double the integer and return it.
+*/
+
+// Fundamentals
+
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i * 2;
+}
