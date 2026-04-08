be575c32a1dc969627a1e497cacfc0ac236812ff
Mujaahid <hackm680@gmail.com>
2026-04-08 16:01:48 +0300

    Remove String Spaces.

---
+++ 32. Challenge-32.js
@@ -0,0 +1,26 @@
+// Remove String Spaces
+
+/*
+Write a function that removes the spaces from the string, then return the resultant string.
+
+Examples (Input -> Output):
+"8 j 8   mBliB8g  imjB8B8  jl  B" -> "8j8mBliB8gimjB8B8jlB"
+"8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd" -> "88Bifk8hB8BB8BBBB888chl8BhBfd"
+"8aaaaa dddd r     " -> "8aaaaaddddr"
+*/
+
+// Fundamentals                Strings
+
+const noSpace = (x) => {
+  
+  // My solution
+  return x.split(" ").join("");
+
+  // Another solution from me.
+  return x.replaceAll(" ", "");
+
+  // Another solution.
+  return x.replace(/\s/g, "");
+};
+
+console.log(noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"));
